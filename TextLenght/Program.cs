3882e1eea813ddc8db9ded4e9a6b0bb3cdf18f5e
dominik1408 <dominik.kasprzak@PK1-12>
2022-04-09 15:29:08 +0200

    dictionary

--- TextLenght/Program.cs
+++ TextLenght/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TextLenght
 {
@@ -6,7 +7,36 @@ namespace TextLenght
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj jakiś tekst");
+
+            string t = Console.ReadLine();
+            string[] text = t.Split(' ');
+
+            Dictionary<int, List<string>> dictionary = new Dictionary<int, List<string>>();
+
+            foreach (var item in text)
+            {
+                int key = item.Length;
+                if(dictionary.ContainsKey(key))
+                {
+                    List<string> list = dictionary[key];
+                    list.Add(item);
+                }
+                else
+                {
+                    List<string> list = new List<string>();
+                    list.Add(item);
+                    dictionary.Add(key, list);
+                }
+            }
+            foreach(KeyValuePair<int, List<string>> kv in dictionary)
+            {
+                Console.WriteLine(kv.Key);
+                foreach (string word in kv.Value)
+                {
+                    Console.WriteLine(word);
+                }
+            }
         }
     }
 }
