a70bcf4c8dede281ad6678fe15d2785796bd324f
dominik1408 <dominik.kasprzak@PK1-12>
2022-04-09 14:45:51 +0200

    Dodaj pliki projektów.

---
+++ TextLenght/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextLenght
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
